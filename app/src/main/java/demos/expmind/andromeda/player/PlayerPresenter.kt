e624cb7c3c4cbca6ad2635ec997ef75cd8ce1214
AjaxianK <e.morphingthrutime@gmail.com>
2019-04-07 22:24:43 -0500

    Remove musixmath api

--- app/src/main/java/demos/expmind/andromeda/player/PlayerPresenter.kt
+++ app/src/main/java/demos/expmind/andromeda/player/PlayerPresenter.kt
@@ -5,17 +5,7 @@ import android.arch.lifecycle.LifecycleObserver
 import android.arch.lifecycle.OnLifecycleEvent
 import android.util.Log
 import com.google.android.youtube.player.YouTubePlayer
-import demos.expmind.andromeda.BuildConfig
-import demos.expmind.andromeda.data.Caption
 import demos.expmind.andromeda.data.Video
-import demos.expmind.andromeda.data.VideoCategory
-import demos.expmind.andromeda.network.LyricsResponse
-import demos.expmind.andromeda.network.LyricsService
-import demos.expmind.network.ServiceGenerator
-import demos.expmind.network.models.ApiKey
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 /**
  * Initializes and controls related operations to a {@link YouTubePlayer}.
@@ -32,12 +22,8 @@ class PlayerPresenter(val view: PlayerContract.View) : PlayerContract.Presenter,
             // PlaylistEventListener, PlaybackEventListener, PlayerStateChangeListener
         }
 
-    val lyricsService: LyricsService
-
     init {
-        ServiceGenerator.setBaseUrl(BuildConfig.MUSIXMATCH_URL, ApiKey("apikey",
-                BuildConfig.MUSIXMATCH_DEVELOPER_KEY))
-        lyricsService = ServiceGenerator.createService(LyricsService::class.java)
+
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_START)
@@ -64,28 +50,6 @@ class PlayerPresenter(val view: PlayerContract.View) : PlayerContract.Presenter,
 
     fun loadVideo(video: Video) {
         player?.loadVideo(video.ytID)
-
-        //TODO this is going to dissapear
-//        lyricsService.search("light up the night", "symphony x").enqueue(object : Callback<LyricsResponse> {
-//            override fun onResponse(call: Call<LyricsResponse>, response: Response<LyricsResponse>) {
-//                if (response.isSuccessful()) {
-//                    //check
-//                    val lyricsDTO: LyricsResponse? = response.body();
-//                    val lyricsText = lyricsDTO?.message?.body?.lyrics?.lyrics_body
-//                    val hugeCaption: Caption = Caption(lyricsText ?: "Not available")
-//                    view.setupSubtitles(listOf(hugeCaption))
-//                } else {
-//                    // handle request errors
-//                    val statusCode = response.code()
-//                }
-//            }
-//
-//            override fun onFailure(call: Call<LyricsResponse>?, t: Throwable?) {
-//                // handle execution failures like no internet connectivity
-//                Log.e(TAG, "error on processing request")
-//            }
-//
-//        })
     }
 
     override fun goToCaption(captionIndex: Int) {
